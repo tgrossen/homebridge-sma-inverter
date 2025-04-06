da94aef9660956b5aa389ad0e084add16d069ce6
Travis Grossen <travis@articulate.com>
2025-04-05 23:48:09 -0600

    debug

--- index.js
+++ index.js
@@ -180,6 +180,7 @@ SMAHomeManager.prototype = {
 					}
 				}
 				else {
+					this.log('Inverter not producing, setting values to 0');
 					this.inverter.getCharacteristic(Characteristic.On).updateValue(false);
 					this.inverter.getCharacteristic(Characteristic.CustomWatts).updateValue(0);
 					this.inverter.getCharacteristic(Characteristic.CurrentAmbientLightLevel).updateValue(0);
